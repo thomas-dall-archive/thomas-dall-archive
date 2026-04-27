a137dd14aae438bade2a8aa2790744334df3ff34
Thomas Dall Archive <279529667+thomas-dall-archive@users.noreply.github.com>
2026-04-27 17:00:34 -0600

    Create update_transcripts.py

---
+++ update_transcripts.py
@@ -0,0 +1,2 @@
+# Command to run via subprocess or shell to get the data
+# yt-dlp --write-auto-subs --skip-download --sub-format vtt --json-pages --flat-playlist https://www.youtube.com/@PotatoOfLife/videos
